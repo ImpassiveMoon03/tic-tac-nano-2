0843755fbc291c73332d45a8c564c550076e34e2
Bob09113 <64452318+Bob09113@users.noreply.github.com>
2020-10-02 05:44:37 -0400

    Added move log that updates with each turn

--- index.js
+++ index.js
@@ -19,10 +19,23 @@ class ticTacNano{
         };
 
         this.turn = function(pos = "A1" || "A2" || "A3" || "B1" || "B2" || "B3" || "C1" || "C2" || "C3",player = "x" || "o"){
+            let b = ''
+            if(player === 'x'){
+                b = xPlayer
+            }else if(player === 'o'){
+                b = oPlayer
+            }else{
+                return false
+            }
             if(this.board[pos] != ''){
+                this.moveLog.push(`${this.moveLog.length+1} - ${b} tried to play an invalid move`)
                 return false
             }
             this.board[pos] = player
+            this.moveLog.push(`${this.moveLog.length+1} - ${b} placed an ${player} on board space ${pos}`)
+            if(this.didWin()){
+                this.moveLog.push(`${this.moveLog.length+1} - ${this.didWin()}`)
+            }
             return true
         };
 
@@ -47,20 +60,20 @@ class ticTacNano{
             }
         
             else if(this.board.A1 === this.board.B1 && this.board.B1 === this.board.C1 && this.board.A1 === 'x'){
-                return `${oPlayer} Wins`
+                return `${xPlayer} Wins`
             }
             else if(this.board.A1 === this.board.B1 && this.board.B1 === this.board.C1 && this.board.A1 === 'o'){
                 return `${oPlayer} Wins`
             }
             else if(this.board.A2 === this.board.B2 && this.board.B2 === this.board.C2 && this.board.A2 === 'x'){
-                return `${oPlayer} Wins`
+                return `${xPlayer} Wins`
             }
             else if(this.board.A2 === this.board.B2 && this.board.B2 === this.board.C2 && this.board.A2 === 'o'){
                 return `${oPlayer} Wins`
             }
         
             else if(this.board.A3 === this.board.B3 && this.board.B3 === this.board.C3 && this.board.A3 === 'x'){
-                return `${oPlayer} Wins`
+                return `${xPlayer} Wins`
             }
             else if(this.board.A3 === this.board.B3 && this.board.B3 === this.board.C3 && this.board.A3 === 'o'){
                 return `${oPlayer} Wins`
@@ -80,9 +93,11 @@ class ticTacNano{
             }
         
             else{
-                return "No winner yet"
+                return false
             }
         }
+
+        this.moveLog = [];
 
         this.v = function(pos){
             if(pos === ''){
